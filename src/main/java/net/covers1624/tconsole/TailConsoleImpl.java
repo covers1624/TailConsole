fe0897a5d36c9e30b16aff475f5a92074bc9082e
covers1624 <laughlan.cov@internode.on.net>
2021-03-19 20:49:45 +1030

    Update to gradle 6.8.3 and Gradle Toolchains, add License headers.
    Remove Guava dependency.

--- src/main/java/net/covers1624/tconsole/TailConsoleImpl.java
+++ src/main/java/net/covers1624/tconsole/TailConsoleImpl.java
@@ -1,6 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-2021 covers1624
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package net.covers1624.tconsole;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import net.covers1624.tconsole.api.TailConsole;
 import net.covers1624.tconsole.api.TailGroup;
 import org.fusesource.jansi.Ansi;
@@ -22,15 +45,15 @@ import java.util.concurrent.*;
 @SuppressWarnings ("SuspiciousMethodCalls")
 public class TailConsoleImpl implements TailConsole {
 
-    private static final ThreadFactory factory = new ThreadFactoryBuilder()//
-            .setDaemon(true)//
-            .setNameFormat("TailConsole Executor")//
-            .build();
-
     private final List<TailLineImpl> tailLines = new ArrayList<>();
     private final List<TailGroupImpl> groups = Collections.synchronizedList(new ArrayList<>());
 
-    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(factory);
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(r -> {
+        Thread thread = new Thread(r);
+        thread.setName("TailConsole Executor");
+        thread.setDaemon(true);
+        return thread;
+    });
 
     private final List<String> stdoutLines = Collections.synchronizedList(new ArrayList<>(32));
 
--- src/main/java/net/covers1624/tconsole/log4j/TailConsoleAppender.java
+++ src/main/java/net/covers1624/tconsole/log4j/TailConsoleAppender.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-2021 covers1624
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package net.covers1624.tconsole.log4j;
 
 import net.covers1624.tconsole.api.TailConsole;
@@ -9,7 +33,6 @@ import org.apache.logging.log4j.core.config.plugins.PluginElement;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
 import org.apache.logging.log4j.core.layout.PatternLayout;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.io.PrintStream;
 import java.io.Serializable;
@@ -47,17 +70,12 @@ public class TailConsoleAppender extends AbstractAppender {
     }
 
     @PluginFactory
-    public static TailConsoleAppender createAppender(//
-            @Required (message = "No name provided for AnsiTailConsoleAppender")//
-            @PluginAttribute ("name")//
-                    String name,//
-            @PluginElement ("Filter")//
-                    Filter filter,//l
-            @PluginElement ("Layout")//
-            @Nullable//
-                    Layout<? extends Serializable> layout,//
-            @PluginAttribute (value = "ignoreExceptions", defaultBoolean = true)//
-                    boolean ignoreExceptions) {
+    public static TailConsoleAppender createAppender(
+            @Required (message = "No name provided for AnsiTailConsoleAppender")
+            @PluginAttribute ("name") String name,
+            @PluginElement ("Filter") Filter filter,
+            @PluginElement ("Layout") Layout<? extends Serializable> layout,
+            @PluginAttribute (value = "ignoreExceptions", defaultBoolean = true) boolean ignoreExceptions) {
         if (layout == null) {
             layout = PatternLayout.createDefaultLayout();
         }
